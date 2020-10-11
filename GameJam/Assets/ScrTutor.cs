616864c06b7031b9dfdd0ca11820c05dbabbbb84
kolty17 <kolty17@gmail.com>
2020-10-11 09:05:59 +0200

    controlli messi nel tutorial
    anche il tutor funziona

---
+++ GameJam/Assets/ScrTutor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScrTutor : MonoBehaviour
+{
+    float timer;
+    public float maxTime = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            timer += Time.deltaTime;            
+        }
+        if (timer > maxTime)
+        {
+            SceneManager.LoadScene("LvGameOver", LoadSceneMode.Single);
+        }       
+    }
+}
